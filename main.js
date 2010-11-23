76a25ecec32fbe3fa1b879d5fbd84cd259329ec2
figital <scott@figital.com>
2010-11-23 16:24:27 -0500

    added addresses

---
+++ main.js
@@ -0,0 +1,62 @@
+function initialize() {
+
+	geocoder = new google.maps.Geocoder();
+
+}
+
+function getCoordinates(lookup, id) {
+
+	geocoder.geocode( { 'address': lookup}, function(results, status) {
+
+		newline = "";
+		previous = $("#tResults").val();		
+
+		if (status == google.maps.GeocoderStatus.OK) {
+
+			loc = results[0].geometry.location;
+			newline = id + ',' + loc + '\n';
+
+		} else {
+			newline = id + ' ERROR: ' + status + '\n';
+		}
+		$('#tResults').val(newline + previous); 
+
+	});
+
+};
+
+
+var geocoder;
+var map;
+
+i = 0;
+datastore = "";
+
+function timedCount() {
+	var l = datastore[i];
+	lookup = l.address1 + ' ' + l.address2 + ', ' + l.city + ', ' + l.state + ' ' + l.zip;
+	getCoordinates(lookup, l.id);			
+	i++;
+	$("#cursor").val(i);
+
+}
+
+function begin() {
+	$.ajax({
+		url: 'locations.json?2',
+		dataType: 'json',
+		async: false,
+		success: function(data){
+			datastore = data; // store globally
+			$("#size").val(datastore.length);
+			setInterval("timedCount()", 5000 );
+		}
+	});
+}
+
+$(document).ready(function($) {
+	
+	initialize();
+
+});
+
